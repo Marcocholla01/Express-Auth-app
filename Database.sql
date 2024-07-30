6b7b9f011db70b05ee640de985e209360fe8f0ad
Marcocholla01 <marcochollapaul01@gmail.com>
2024-07-30 12:52:31 +0300

    Project Ready for depoyment

--- Database.sql
+++ Database.sql
@@ -1,10 +1,15 @@
 CREATE TABLE IF NOT EXISTS users (
     userId VARCHAR(25) NOT NULL PRIMARY KEY,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(5000) NOT NULL,
+    contact VARCHAR(20) NOT NULL,
     email VARCHAR(255) NOT NULL,
     isVerified TINYINT(1) DEFAULT 0,
     isActive TINYINT(1) DEFAULT 1,
     role ENUM('user', 'admin') DEFAULT 'user',
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP,
     UNIQUE (email)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+);
